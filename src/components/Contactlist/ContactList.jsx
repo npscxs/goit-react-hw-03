781ec2ec2341cbd32107d6e2747b875bedbcb455
npscxs <npscxs@gmail.com>
2025-03-11 13:03:53 +0100

    add komponents

---
+++ src/components/Contactlist/ContactList.jsx
@@ -0,0 +1,4 @@
+import Contact from "../Contact/Contact";
+export default function ContactList() {
+  return <Contact />;
+}
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,3 @@
+export default function SearchBox() {
+  return <p>Search box</p>;
+}
